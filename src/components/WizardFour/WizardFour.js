491516b716596afd5a0c06a5709a7ed62f08eaf0
Andrew Montgomery <wamontgomery0068@gmail.com>
2018-10-15 17:53:32 -0500

    Completed

--- src/components/WizardFour/WizardFour.js
+++ src/components/WizardFour/WizardFour.js
@@ -1,6 +1,9 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+import { updateFound } from '../../ducks/reducer.js';
+
 class WizardFour extends Component {
     render(){
         return(
@@ -9,8 +12,8 @@ class WizardFour extends Component {
                     <p>Have you already found your new home?</p> <br />
                     
                     <div className="row">
-                        <Link to="/wFive"><button onClick={this.props.foundTrue}>Yes</button></Link>
-                        <Link to="/wFive"><button onClick={this.props.foundFalse}>No </button></Link>  
+                        <Link to="/wFive"><button onClick={ (e)=>this.props.updateFound("True")}>Yes</button></Link>
+                        <Link to="/wFive"><button onClick={ (e)=>this.props.updateFound("False")}>No </button></Link>  
                     </div>         
                 </div>
             </div>
@@ -18,4 +21,12 @@ class WizardFour extends Component {
     }
 }
 
-export default WizardFour;
+function mapStateToProps( state ) {
+    const { found } = state;
+
+    return {
+        found
+    };
+}
+
+export default connect(mapStateToProps, { updateFound } ) (WizardFour);
--- src/components/WizardSeven/WizardSeven.js
+++ src/components/WizardSeven/WizardSeven.js
@@ -1,20 +1,35 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+import { updateCredit } from '../../ducks/reducer';
+
 class WizardSeven extends Component {
 
     render(){
+        const { updateCredit } = this.props;
+        
         return(
             <div className="parent-div">
                 <div className="vert-align">                    
-                    
                     <p>Estimate your credit score</p> <br />
                     
                     <div className="row">
-                        <Link to="/wEight"><button onClick={this.props.updateCreditE}>Excellent</button></Link>
-                        <Link to="/wEight"><button onClick={this.props.updateCreditG}>Good</button></Link>
-                        <Link to="/wEight"><button onClick={this.props.updateCreditF}>Fair</button></Link>
-                        <Link to="/wEight"><button onClick={this.props.updateCreditP}>Poor</button></Link>
+                        <Link to="/wEight">
+                        <button onClick={ ()=> updateCredit('Excellent') }>Excellent</button>
+                        </Link>
+
+                        <Link to="/wEight">
+                        <button onClick={ ()=> updateCredit('Good') }>Good</button>
+                        </Link>
+
+                        <Link to="/wEight">
+                        <button onClick={ ()=> updateCredit ('Fair') }>Fair</button>
+                        </Link>
+
+                        <Link to="/wEight">
+                        <button onClick={ ()=> updateCredit ('Poor') }>Poor</button>
+                        </Link>
                     </div>
                 </div>
             </div>
@@ -22,4 +37,12 @@ class WizardSeven extends Component {
     }
 }
 
-export default WizardSeven;
+function mapStateProps( state ) {
+    const { credit } = state;
+
+    return {
+        credit
+    };
+}
+
+export default connect( mapStateProps, { updateCredit } ) (WizardSeven);
